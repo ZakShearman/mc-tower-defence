929cd0fb30c656868a7ea4e9d4aac0eb93970e22
Zak <shearmanzak@gmail.com>
2021-09-19 12:57:40 +0100

    Clean up some event classes

--- src/main/java/pink/zak/minestom/towerdefence/api/event/game/TowerDamageEvent.java
+++ src/main/java/pink/zak/minestom/towerdefence/api/event/game/TowerDamageEvent.java
@@ -3,6 +3,5 @@ package pink.zak.minestom.towerdefence.api.event.game;
 import net.minestom.server.event.Event;
 import pink.zak.minestom.towerdefence.enums.Team;
 
-public record TowerDamageEvent(Team team, int damage,
-                               int health) implements Event {
+public record TowerDamageEvent(Team team, int damage, int health) implements Event {
 }
--- src/main/java/pink/zak/minestom/towerdefence/api/event/player/PlayerTeamSwitchEvent.java
+++ src/main/java/pink/zak/minestom/towerdefence/api/event/player/PlayerTeamSwitchEvent.java
@@ -2,16 +2,9 @@ package pink.zak.minestom.towerdefence.api.event.player;
 
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.Event;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import pink.zak.minestom.towerdefence.enums.Team;
 
 public record PlayerTeamSwitchEvent(Team joinedTeam,
                                     Team oldTeam,
                                     Player player) implements Event {
-    public PlayerTeamSwitchEvent(@Nullable Team joinedTeam, @Nullable Team oldTeam, @NotNull Player player) {
-        this.joinedTeam = joinedTeam;
-        this.oldTeam = oldTeam;
-        this.player = player;
-    }
 }
