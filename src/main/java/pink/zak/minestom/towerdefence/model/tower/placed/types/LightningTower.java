7971ba8dbcad6949a7c294e5ced2720d6ceab19a
Zak <shearmanzak@gmail.com>
2021-12-31 02:03:46 +0000

    Begin adding guardian beam

--- src/main/java/pink/zak/minestom/towerdefence/model/tower/placed/types/LightningTower.java
+++ src/main/java/pink/zak/minestom/towerdefence/model/tower/placed/types/LightningTower.java
@@ -1,31 +1,33 @@
 package pink.zak.minestom.towerdefence.model.tower.placed.types;
 
-import com.google.common.collect.Lists;
+import net.kyori.adventure.text.Component;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.adventure.audience.Audiences;
 import net.minestom.server.coordinate.Point;
+import net.minestom.server.entity.EntityCreature;
+import net.minestom.server.entity.EntityType;
+import net.minestom.server.entity.LivingEntity;
 import net.minestom.server.entity.Player;
+import net.minestom.server.entity.metadata.monster.GuardianMeta;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.item.Material;
 import net.minestom.server.network.packet.server.SendablePacket;
 import net.minestom.server.particle.Particle;
 import net.minestom.server.particle.ParticleCreator;
 import net.minestom.server.utils.Direction;
-import pink.zak.minestom.towerdefence.enums.Team;
-import pink.zak.minestom.towerdefence.game.GameHandler;
-import pink.zak.minestom.towerdefence.game.MobHandler;
 import pink.zak.minestom.towerdefence.model.GameUser;
 import pink.zak.minestom.towerdefence.model.mob.living.LivingEnemyMob;
 import pink.zak.minestom.towerdefence.model.tower.config.Tower;
 import pink.zak.minestom.towerdefence.model.tower.config.towers.LightningTowerLevel;
 import pink.zak.minestom.towerdefence.model.tower.placed.PlacedAttackingTower;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class LightningTower extends PlacedAttackingTower<LightningTowerLevel> {
+    private final List<LivingEntity> guardians = new CopyOnWriteArrayList<>();
     private Point castPoint;
 
     public LightningTower(Instance instance, Tower tower, Material towerPlaceMaterial, short id, GameUser owner, Point basePoint, Direction facing, int level) {
@@ -40,9 +42,21 @@ public class LightningTower extends PlacedAttackingTower<LightningTowerLevel> {
 
     @Override
     protected void fire() {
-        this.drawParticles();
-        for (LivingEnemyMob enemyMob : targets) {
+        if (this.level.getLevel() > 2)
+            this.drawGuardianBeams();
+        else
+            this.drawParticles();
+
+        for (LivingEnemyMob enemyMob : this.targets)
             enemyMob.towerDamage(this, this.level.getDamage());
+    }
+
+    private void drawGuardianBeams() {
+        for (int i = 0; i < this.targets.size(); i++) {
+            LivingEnemyMob target = this.targets.get(i);
+            LivingEntity guardian = this.guardians.get(i);
+            ((GuardianMeta) guardian.getEntityMeta()).setTarget(target);
+            Audiences.all().sendMessage(Component.text(System.nanoTime() + " set the target of a guardian beam"));
         }
     }
 
@@ -88,7 +102,26 @@ public class LightningTower extends PlacedAttackingTower<LightningTowerLevel> {
 
     @Override
     public void upgrade() {
+        LightningTowerLevel oldLevel = this.getLevel();
+
         super.upgrade();
         this.castPoint = this.getLevel().getRelativeCastPoint().apply(this.getBasePoint());
+        this.updateGuardianBeams(oldLevel);
+    }
+
+    private void updateGuardianBeams(LightningTowerLevel oldLevel) {
+        if (this.level.getLevel() > 2) {
+            int beamDifference = this.level.getMaxTargets() - this.guardians.size();
+            if (beamDifference > 0) {
+                for (int i = 0; i < beamDifference; i++) {
+                    EntityCreature guardian = new EntityCreature(EntityType.GUARDIAN);
+                    guardian.setInvisible(true);
+                    this.guardians.add(guardian);
+                }
+            } else if (beamDifference < 0) { // maybe someone will configure it to do this, probably not but let's just handle it.
+                LivingEntity removedGuardian = this.guardians.remove(oldLevel.getMaxTargets() - 1);
+                removedGuardian.kill();
+            }
+        }
     }
 }
