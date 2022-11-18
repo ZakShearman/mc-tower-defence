87667667fc5dee5c6dbf67b66054eb35429784fd
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2022-11-18 10:30:15 +0000

    Set main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("org.sonarqube") version "3.4.0.2513"
+    id("application")
 }
 
 group = "pink.zak.minestom"
@@ -26,45 +26,11 @@ dependencies {
     //testImplementation("net.minestom.server:Minestom:1.0")
 }
 
+application {
+    mainClass.set("pink.zak.minestom.towerdefence.TowerDefenceServer")
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
-}
-
-tasks.register("depsize") {
-    description = "Prints dependencies for \"default\" configuration"
-    doLast {
-        listConfigurationDependencies(configurations["default"])
-    }
-}
-
-tasks.register("depsize-all-configurations") {
-    description = "Prints dependencies for all available configurations"
-    doLast {
-        configurations
-            .filter { it.isCanBeResolved }
-            .forEach { listConfigurationDependencies(it) }
-    }
-}
-
-fun listConfigurationDependencies(configuration: Configuration) {
-    val formatStr = "%,10.2f"
-
-    val size = configuration.map { it.length() / (1024.0 * 1024.0) }.sum()
-
-    val out = StringBuffer()
-    out.append("\nConfiguration name: \"${configuration.name}\"\n")
-    if (size > 0) {
-        out.append("Total dependencies size:".padEnd(65))
-        out.append("${String.format(formatStr, size)} Mb\n\n")
-
-        configuration.sortedBy { -it.length() }
-            .forEach {
-                out.append(it.name.padEnd(65))
-                out.append("${String.format(formatStr, (it.length() / 1024.0))} kb\n")
-            }
-    } else {
-        out.append("No dependencies found")
-    }
-    println(out)
 }
