ccb937c27679ef9b76f15ba16690e19b0a804089
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-05-03 15:36:20 +0100

    Set UTF-8 encoding on gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,10 @@ tasks.named<ShadowJar>("shadowJar") {
     mergeServiceFiles()
 }
 
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
