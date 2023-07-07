ab7a34116a1c1cfc251d8c19fae2788615636e91
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-07-08 00:07:07 +0100

    Fix oopsie

--- src/main/java/pink/zak/minestom/towerdefence/TowerDefenceServer.java
+++ src/main/java/pink/zak/minestom/towerdefence/TowerDefenceServer.java
@@ -7,7 +7,7 @@ import dev.emortal.minestom.core.module.monitoring.MonitoringModule;
 public class TowerDefenceServer {
 
     public static void main(String[] args) {
-        new MinestomServer.Builder()
+        MinestomServer.builder()
                 .commonModules()
                 .module(MonitoringModule.class, MonitoringModule::new)
                 .module(TowerDefenceModule.class, TowerDefenceModule::new)
