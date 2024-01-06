644c0b4b745a6c8f85a949278f65d33103823be2
Luis <luis@lu15.dev>
2024-01-06 19:31:23 +0000

    feat(targetting): use mobs' bounding boxes instead of their positions for distance checks

--- src/main/java/pink/zak/minestom/towerdefence/model/tower/placed/PlacedAttackingTower.java
+++ src/main/java/pink/zak/minestom/towerdefence/model/tower/placed/PlacedAttackingTower.java
@@ -3,6 +3,7 @@ package pink.zak.minestom.towerdefence.model.tower.placed;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import net.minestom.server.collision.BoundingBox;
 import net.minestom.server.coordinate.Point;
 import net.minestom.server.event.instance.InstanceTickEvent;
 import net.minestom.server.instance.Instance;
@@ -51,8 +52,34 @@ public abstract class PlacedAttackingTower<T extends AttackingTowerLevel> extend
                 // filter out mobs that are predicted to be dead before the tower can fire
                 .filter(mob -> !mob.isPredictedDead())
                 // filter out mobs that are out of the tower's range
-                .filter(mob -> mob.getPosition().distance(this.getBasePoint()) <= this.level.getRange())
-                .collect(Collectors.toUnmodifiableSet());
+                .filter(mob -> {
+                    // get the 4 points of the mob's bounding box
+                    BoundingBox boundingBox = mob.getBoundingBox();
+                    double minX = boundingBox.minX();
+                    double maxX = boundingBox.maxX();
+                    double minY = boundingBox.minY();
+                    double maxY = boundingBox.maxY();
+                    double minZ = boundingBox.minZ();
+                    double maxZ = boundingBox.maxZ();
+
+                    // get current mob position
+                    Point position = mob.getPosition();
+
+                    // convert the points to a set
+                    Set<Point> points = Set.of(
+                            position.add(minX, minY, minZ),
+                            position.add(minX, minY, maxZ),
+                            position.add(minX, maxY, minZ),
+                            position.add(minX, maxY, maxZ),
+                            position.add(maxX, minY, minZ),
+                            position.add(maxX, minY, maxZ),
+                            position.add(maxX, maxY, minZ),
+                            position.add(maxX, maxY, maxZ)
+                    );
+
+                    // check if any of the points are within the tower's range
+                    return points.stream().anyMatch(point -> point.distance(this.getBasePoint()) <= this.level.getRange());
+                }).collect(Collectors.toUnmodifiableSet());
     }
 
     public @NotNull List<LivingTDEnemyMob> findPossibleTargets(@NotNull Target target) {
