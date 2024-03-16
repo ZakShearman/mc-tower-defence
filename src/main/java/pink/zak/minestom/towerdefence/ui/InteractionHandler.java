9bd3089919a5199fde3ebcfcc7bc92e1fe858869
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2024-03-16 11:07:54 +0000

    fix: allow blocks to be right clicked

--- src/main/java/pink/zak/minestom/towerdefence/ui/InteractionHandler.java
+++ src/main/java/pink/zak/minestom/towerdefence/ui/InteractionHandler.java
@@ -1,71 +1,80 @@
 package pink.zak.minestom.towerdefence.ui;
 
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Point;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.EventFilter;
 import net.minestom.server.event.EventNode;
-import net.minestom.server.event.player.PlayerBlockInteractEvent;
-import net.minestom.server.event.player.PlayerTickEvent;
 import net.minestom.server.event.trait.PlayerEvent;
-import net.minestom.server.instance.block.Block;
 import net.minestom.server.item.ItemStack;
+import net.minestom.server.timer.TaskSchedule;
 import org.jetbrains.annotations.NotNull;
 import pink.zak.minestom.towerdefence.TowerDefenceModule;
-import pink.zak.minestom.towerdefence.enums.GameState;
 import pink.zak.minestom.towerdefence.enums.TowerType;
+import pink.zak.minestom.towerdefence.game.GameHandler;
 import pink.zak.minestom.towerdefence.model.tower.placed.PlacedTower;
 import pink.zak.minestom.towerdefence.model.user.GameUser;
-import pink.zak.minestom.towerdefence.ui.tower.TowerManagementUI;
 import pink.zak.minestom.towerdefence.ui.tower.TowerOutliner;
 import pink.zak.minestom.towerdefence.ui.tower.TowerPlaceUI;
+import pink.zak.minestom.towerdefence.world.TowerDefenceInstance;
 
 public final class InteractionHandler {
 
     private final @NotNull EventNode<PlayerEvent> eventNode = EventNode.type("hotbar-handler", EventFilter.PLAYER);
     private final @NotNull EventNode<? super PlayerEvent> parentNode;
+    private final @NotNull GameHandler gameHandler;
+    private final @NotNull TowerDefenceInstance instance;
 
-    public InteractionHandler(@NotNull TowerDefenceModule module, @NotNull EventNode<? super PlayerEvent> node) {
+    public InteractionHandler(@NotNull TowerDefenceModule module, @NotNull GameHandler gameHandler, @NotNull EventNode<? super PlayerEvent> node) {
         this.parentNode = node;
-        this.eventNode.addListener(PlayerBlockInteractEvent.class, event -> {
-            if (event.getHand() != Player.Hand.MAIN) return;
-            if (module.getGameState() != GameState.GAME) return;
+        this.gameHandler = gameHandler;
+        this.instance = module.getInstance();
 
-            Player player = event.getPlayer();
-            GameUser user = module.getGameHandler().getGameUser(player);
-            if (user == null) throw new IllegalStateException("Player is not associated with a game user");
+//        this.eventNode.addListener(PlayerBlockInteractEvent.class, event -> {
+//            if (event.getHand() != Player.Hand.MAIN) return;
+//            if (module.getGameState() != GameState.GAME) return;
+//
+//            Player player = event.getPlayer();
+//            GameUser user = module.getGameHandler().getGameUser(player);
+//            if (user == null) throw new IllegalStateException("Player is not associated with a game user");
+//
+//            Block block = event.getBlock();
+//
+//            // check if clicked block is a tower. if so, open the tower management UI
+//            if (block.hasTag(PlacedTower.ID_TAG)) {
+//                int id = block.getTag(PlacedTower.ID_TAG);
+//                PlacedTower<?> tower = module.getGameHandler().getTowerManager().getTower(user.getTeam(), id);
+//                if (tower == null)
+//                    throw new IllegalStateException("Player attempted to interact with a tower that does not exist");
+//                player.openInventory(new TowerManagementUI(tower, user, module.getGameHandler().getTowerManager()));
+//            }
+//        });
 
-            Block block = event.getBlock();
-
-            // check if clicked block is a tower. if so, open the tower management UI
-            if (block.hasTag(PlacedTower.ID_TAG)) {
-                int id = block.getTag(PlacedTower.ID_TAG);
-                PlacedTower<?> tower = module.getGameHandler().getTowerManager().getTower(user.getTeam(), id);
-                if (tower == null) throw new IllegalStateException("Player attempted to interact with a tower that does not exist");
-                player.openInventory(new TowerManagementUI(tower, user, module.getGameHandler().getTowerManager()));
-            }
-        });
-
-        TowerOutliner outliner = new TowerOutliner(module.getInstance());
-        this.eventNode.addListener(PlayerTickEvent.class, event -> {
-            Player player = event.getPlayer();
-            GameUser user = module.getGameHandler().getGameUser(player);
-            if (user == null) throw new IllegalStateException("Player is not associated with a game user");
-
-            ItemStack item = player.getItemInMainHand();
-            if (!item.hasTag(TowerPlaceUI.UI_TAG)) return;
-
-            TowerType towerType = item.getTag(TowerPlaceUI.TOWER_TYPE);
-            if (towerType == null) return;
-
-            Point targetBlockPos = player.getTargetBlockPosition(TowerPlaceUI.TOWER_PLACE_DISTANCE);
-            if (targetBlockPos == null) return;
-
-            player.sendPackets(outliner.calculateOutline(user, targetBlockPos, towerType));
-        });
     }
 
     public void initialise() {
         this.parentNode.addChild(this.eventNode);
+
+        TowerOutliner outliner = new TowerOutliner(this.instance);
+        MinecraftServer.getSchedulerManager().buildTask(() -> {
+            for (Player player : MinecraftServer.getConnectionManager().getOnlinePlayers()) {
+                GameUser user = this.gameHandler.getGameUser(player);
+                if (user == null) throw new IllegalStateException("Player is not associated with a game user");
+
+                ItemStack item = player.getItemInMainHand();
+                if (!item.hasTag(TowerPlaceUI.UI_TAG)) return;
+
+                TowerType towerType = item.getTag(TowerPlaceUI.TOWER_TYPE);
+                if (towerType == null) return;
+
+                Point targetBlockPos = player.getTargetBlockPosition(TowerPlaceUI.TOWER_PLACE_DISTANCE);
+                if (targetBlockPos == null) return;
+
+                if (player.getInstance().getBlock(targetBlockPos).hasTag(PlacedTower.ID_TAG)) return;
+
+                player.sendPackets(outliner.calculateOutline(user, targetBlockPos, towerType));
+            }
+        }).repeat(TaskSchedule.tick(2)).schedule();
     }
 
     public void shutdown() {
